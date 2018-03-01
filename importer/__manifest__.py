e2d866232d61203254ad12638bae43c47d27083e
SebastianPardo <jspardoj@unal.edu.co>
2018-02-28 22:25:58 -0500

    jspardoj@unal.edu.co

---
+++ importer/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2016 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Importer",
+    "summary": "Import",
+    "version": "10.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "http://cti.com/",
+    "author": "CTI Ltda",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["base"],
+    "data": [
+        "views/importer_view.xml",
+    ],
+    "qweb": [
+        # "static/src/xml/date_range.xml",
+    ]
+}
